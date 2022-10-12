1599141819d7046ddbd24ecadf6f3c6f55f5d288
Sen Pan <sen@hypevr.com>
2022-10-12 16:56:02 -0700

    update eigen

--- 3rdparty/eigen/eigen.cmake
+++ 3rdparty/eigen/eigen.cmake
@@ -4,8 +4,8 @@ ExternalProject_Add(
     ext_eigen
     PREFIX eigen
     # Commit point: https://gitlab.com/libeigen/eigen/-/merge_requests/716
-    URL https://gitlab.com/libeigen/eigen/-/archive/da7909592376c893dabbc4b6453a8ffe46b1eb8e/eigen-da7909592376c893dabbc4b6453a8ffe46b1eb8e.tar.gz
-    URL_HASH SHA256=37f71e1d7c408e2cc29ef90dcda265e2de0ad5ed6249d7552f6c33897eafd674
+    URL https://drive.google.com/uc?id=14Q5RRHGoif99NeP2QQth6xGhBpfQ7VH0&export=download
+    URL_HASH SHA256=0a558164b4ebb2fcd773758691ba73a610725335008a8f0e2b2cebfb41ea556c
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/eigen"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
